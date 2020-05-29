afc8cd6cd01137049b6cb421e521591adc55857d
Glitch (hello-webpage) <none>
2020-05-29 23:00:40 +0000

    👗🍐 Checkpoint ./three/examples/index.js:15232297/4608 ./index.html:15232297/3339

--- three/examples/index.js
+++ three/examples/index.js
@@ -1,80 +1,84 @@
-import * as THREE from '../build/three.module.js';
+import * as THREE from "../build/three.module.js";
 /*  */
-    let camera, scene, renderer, sphere, clock;
+let camera, scene, renderer, sphere, clock;
 
-    init();
-    animate();
+init();
+animate();
 
-    function init() {
+function init() {
+  let container = document.getElementById("container");
 
-      let container = document.getElementById('container');
+  clock = new THREE.Clock();
 
-      clock = new THREE.Clock();
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x101010);
 
-      scene = new THREE.Scene();
-      scene.background = new THREE.Color(0x101010);
+  let light = new THREE.AmbientLight(0xffffff, 1);
+  scene.add(light);
 
-      let light = new THREE.AmbientLight(0xffffff, 1);
-      scene.add(light);
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    1,
+    2000
+  );
+  scene.add(camera);
 
-      camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 2000);
-      scene.add(camera);
+  // Create the panoramic sphere geometery
+  let panoSphereGeo = new THREE.SphereBufferGeometry(6, 256, 256);
 
-      // Create the panoramic sphere geometery
-      let panoSphereGeo = new THREE.SphereBufferGeometry(6, 256, 256);
+  // Create the panoramic sphere material
+  let panoSphereMat = new THREE.MeshStandardMaterial({
+    side: THREE.BackSide,
+    displacementScale: -4.0
+  });
 
-      // Create the panoramic sphere material
-      let panoSphereMat = new THREE.MeshStandardMaterial(
-        {
-          side: THREE.BackSide,
-          displacementScale: -4.0
-        });
+  sphere = new THREE.Mesh(panoSphereGeo, panoSphereMat);
 
-      sphere = new THREE.Mesh(panoSphereGeo, panoSphereMat);
+  // Load and assign the texture and depth map
+  let manager = new THREE.LoadingManager();
+  let loader = new THREE.TextureLoader(manager);
 
-      // Load and assign the texture and depth map
-      let manager = new THREE.LoadingManager();
-      let loader = new THREE.TextureLoader(manager);
-
-      loader.load('https://cdn.glitch.com/48664c52-5883-4696-9f8c-4527b79e49d2%2Fvirus-4937553.jpg?v=1590786816007', function (texture) {
-        texture.minFilter = THREE.NearestFilter;
-        texture.generateMipmaps = false;
-        sphere.material.map = texture;
-      });
-
-      manager.onLoad = function () {
-        scene.add(sphere);
-      };
-
-      renderer = new THREE.WebGLRenderer();
-      renderer.setPixelRatio(window.devicePixelRatio);
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      renderer.xr.enabled = true;
-      renderer.xr.setReferenceSpaceType('local');
-      container.appendChild(renderer.domElement);
-
- /*      document.body.appendChild(VRButton.createButton(renderer)); */
-      window.addEventListener('resize', onWindowResize, false);
+  loader.load(
+    "https://cdn.glitch.com/48664c52-5883-4696-9f8c-4527b79e49d2%2Fvirus-4937553.jpg?v=1590786816007",
+    function(texture) {
+      texture.minFilter = THREE.NearestFilter;
+      texture.generateMipmaps = false;
+      sphere.material.map = texture;
     }
+  );
 
-    function onWindowResize() {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      renderer.setSize(window.innerWidth, window.innerHeight);
-    }
-    function animate() {
-      renderer.setAnimationLoop(render);
-    }
+  manager.onLoad = function() {
+    scene.add(sphere);
+  };
 
-    function render() {
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.xr.enabled = true;
+  renderer.xr.setReferenceSpaceType("local");
+  container.appendChild(renderer.domElement);
 
-      if (renderer.xr.isPresenting === false) {
+  /*      document.body.appendChild(VRButton.createButton(renderer)); */
+  window.addEventListener("resize", onWindowResize, false);
+}
 
-        let time = clock.getElapsedTime();
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+function animate() {
+  renderer.setAnimationLoop(render);
+}
 
-        sphere.rotation.y += 0.001;
-        sphere.position.x = Math.sin(time) * 0.2;
-        sphere.position.z = Math.cos(time) * 0.2;
-      }
+function render() {
+  if (renderer.xr.isPresenting === false) {
+    let time = clock.getElapsedTime();
 
-      renderer.render(scene, camera);
-    }
+    sphere.rotation.y += 0.001;
+    sphere.position.x = Math.sin(time) * 0.2;
+    sphere.position.z = Math.cos(time) * 0.2;
+  }
+
+  renderer.render(scene, camera);
+}
